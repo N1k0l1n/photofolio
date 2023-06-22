693c84b29fc4314246d43b4699d518fc3a8ecbcc
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-06-22 11:15:33 +0200

    hero + navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
-import { Raleway } from 'next/font/google'
-import Head from 'next/head'
+import Hero from "@/components/Hero";
+import { Raleway } from "next/font/google";
+import Head from "next/head";
 
-const raleway = Raleway({ subsets: ['latin'] })
+const raleway = Raleway({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${raleway.className}`}
-    >
+    <div className="raleway">
       <Head>
         <title>PhotoFolio Next App</title>
       </Head>
+      <Hero heading="PhotoFolio" message="Where Beauty Comes Alive" />
     </div>
-  )
+  );
 }
